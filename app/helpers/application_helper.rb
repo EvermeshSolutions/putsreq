74fb5bbda5d4a4afa8922aa8c645475609a8eb6c
Andrew Hooker <geekoncoffee@gmail.com>
2014-05-14 22:30:12 -0500

    indentation fixes, helper refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     controller_name = controller_path.gsub(/\//, "_")
     "#{controller_name}##{action_name}"
   end
+
+  def token_url(request, token)
+    request.url.gsub(request.path, '') + "/" + token
+  end
 end
