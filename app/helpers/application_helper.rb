f7c3db08f4e6d01c4b1aeb0b63d8a7c1876fd146
Pablo Cantero <pablo@pablocantero.com>
2014-09-23 22:42:34 -0300

    Show a specific No Request Found when the requests were expired

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
   def token_url(token)
     "#{request.protocol}#{request.host_with_port}/#{token}"
   end
+
+  def show_no_requests_found(bucket)
+    if bucket.requests_count > 0
+      content_tag(:p, "Oops... You received #{bucket.requests_count} requests, but they were already expired. Make more requests!")
+    else
+      content_tag(:p, 'No requests received. Run the cURL above to start having fun!')
+    end
+  end
 end
