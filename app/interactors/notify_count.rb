e4c4bf60faeb7bcdfcf1e31c957c5acb4583f766
Pablo Cantero <pablo@pablocantero.com>
2015-08-01 13:25:10 -0300

    Try using bucket.id instead of bucket.token as a Pusher channel to avoid "Pusher::Error (Illegal channel name 'channel_emails_something+1438443176938'):"

--- app/interactors/notify_count.rb
+++ app/interactors/notify_count.rb
@@ -6,12 +6,8 @@ class NotifyCount
 
     Pusher.url = "http://3466d56fe2ef1fdd2943:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV['PUSHER_APP_ID']}"
 
-    Pusher["channel_requests_#{bucket.token}"].trigger 'update_count', bucket.requests_count
-  end
-
-  private
-
-  def bucket
-    context.bucket
+    Pusher["channel_requests_#{context.bucket.id}"].trigger 'update_count', context.bucket.requests_count
+  rescue => e
+    Rails.logger.error(e)
   end
 end
