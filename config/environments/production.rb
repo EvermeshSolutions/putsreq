479680fd43d08a71785c1198e9b8bd9cd40748f2
Pablo Cantero <pablo@pablocantero.com>
2015-08-02 08:52:00 -0300

    Add default_url_options to fix reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ PutsReq::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'putsreq.com' }
 end
