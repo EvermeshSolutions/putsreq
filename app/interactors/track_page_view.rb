688e4661a42ad6bdf366dde7a79fb58d089f6107
Pablo Cantero <pablo@pablocantero.com>
2015-08-08 10:37:35 -0300

    Test TrackPageView with events

--- app/interactors/track_page_view.rb
+++ app/interactors/track_page_view.rb
@@ -2,11 +2,25 @@ class TrackPageView
   include Interactor
 
   def call
-    return unless ENV['GA'] || ENV['GA_HOSTNAME']
+    return unless ENV['GA']
 
-    tracker = Staccato.tracker(ENV['GA'])
+    options = {
+      hostname:    rack_request.host,
+      path:        rack_request.path,
+      user_ip:     rack_request.remote_ip,
+      user_agent:  rack_request.user_agent,
+      referrer:    rack_request.referer
+    }
 
-    tracker.pageview(path: "/#{bucket.token}", hostname: ENV['GA_HOSTNAME'], title: bucket.name)
+    tracker = Staccato.tracker(ENV['GA'], nil, options)
+
+    tracker.pageview(title: bucket.name)
+
+    event = tracker.build_event(category: 'Requests', action: 'record', non_interactive: true)
+
+    event.add_measurement(:request, token: bucket.token)
+
+    event.track!
   rescue => e
     Rails.logger.error(e)
   end
@@ -15,5 +29,9 @@ class TrackPageView
 
   def bucket
     context.bucket
+  end
+
+  def rack_request
+    context.rack_request
   end
 end
