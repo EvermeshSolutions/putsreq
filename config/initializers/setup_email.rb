7a19f4b6dcfe19a572200fc3684e00b04d7c643f
Pablo Cantero <pablo@pablocantero.com>
2016-03-20 10:24:49 -0300

    Replace Mandrill with SendGrid

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,11 +1,12 @@
-if ENV['MANDRILL_USERNAME'] && ENV['MANDRILL_APIKEY']
+if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
   ActionMailer::Base.smtp_settings = {
-    port:            '587',
-    address:         'smtp.mandrillapp.com',
-    user_name:       ENV['MANDRILL_USERNAME'],
-    password:        ENV['MANDRILL_APIKEY'],
-    domain:          'heroku.com',
-    authentication:  :plain
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
 end
