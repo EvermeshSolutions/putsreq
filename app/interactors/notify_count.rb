a0457a4dc606883e83ed0efcc338624cfa91f6bf
Pablo Cantero <pablo@pablocantero.com>
2016-11-27 15:41:11 -0500

    Send the full request in the pusher message

--- app/interactors/notify_count.rb
+++ app/interactors/notify_count.rb
@@ -6,7 +6,9 @@ class NotifyCount
   def call
     return unless ENV['PUSHER_URL']
 
-    Pusher["channel_requests_#{bucket.id}"].trigger 'update_count', bucket.requests_count
+    Pusher["channel_requests_#{bucket.token}"].trigger('new',
+                                                       count: bucket.requests_count,
+                                                       request: built_request)
   rescue => e
     Rails.logger.error(e)
   end
