dc20ddee0a38302eef4eb5a3132d59c3c4dc8cdc
Pablo Cantero <pablo@pablocantero.com>
2016-11-27 21:39:50 -0500

    Register only user supplied headers

--- app/interactors/filter_headers.rb
+++ app/interactors/filter_headers.rb
@@ -12,14 +12,12 @@ class FilterHeaders
   def client_supplied_headers
     headers.to_h.each_with_object({}) do |(key, value), h|
       next unless value.to_s.present?
-      next unless key.upcase == key
+      # Only saves user supplied headers HTTP_ or content-type/length
+      next unless key.start_with?('HTTP_') || %w(content-type content-length).include?(key.downcase)
 
       key = key.sub('HTTP_', '').tr('_', '-')
 
-      next if key.upcase.start_with? 'CF-' # Ignore CloudFlare headers
-      next if key.upcase.start_with? 'SERVER-'
-
-      next if %w(host transfer-encoding).include? key.downcase
+      next if key.start_with? 'CF-' # ignore CloudFare headers
 
       h[key] = value
     end
