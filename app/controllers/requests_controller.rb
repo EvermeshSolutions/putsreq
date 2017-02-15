0b08730f6dec1cbaca82bbb91ef22bfb30626be0
Pablo Cantero <pablo@pablocantero.com>
2017-02-15 09:58:30 -0500

    Allow viewing request by id

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,10 @@
+class RequestsController < ApplicationController
+  def show
+    request = Request.find(params[:id])
+
+    respond_to do |format|
+      format.html { render text: request.body }
+      format.json { render json: JSON.pretty_generate(request.attributes) }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ PutsReq::Application.routes.draw do
   post 'buckets' => 'buckets#create', as: :buckets
   get ':token/inspect' => 'buckets#show', as: :bucket
   get ':token/last' => 'buckets#last', as: :bucket_last
+  get ':token/requests/:id' => 'requests#show'
   get ':token/last_response' => 'buckets#last_response', as: :bucket_last_response
   put ':token/buckets' => 'buckets#update', as: :update_bucket
   match ':token' => 'buckets#record', via: :all, as: :bucket_record
