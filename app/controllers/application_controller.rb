8a3afb12ec2a2c085200f39f2e0f0e3e084fc69d
Pablo Cantero <pablo@pablocantero.com>
2017-02-23 21:28:33 -0500

    :lipstick:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :is_owner?, :body_as_string, :headers_as_string
+  helper_method :owner?, :body_as_string, :headers_as_string
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -41,21 +41,19 @@ class ApplicationController < ActionController::Base
 
   def body_json?(req_or_res)
     req_or_res.headers.to_h.each do |key, value|
-      if key =~ /^content-type$/i
-        return !!(value =~ /application\/json/i)
-      end
+      return !!(value =~ /application\/json/i) if key =~ /^content-type$/i
     end
 
     false
   end
 
   def check_ownership!
-    unless is_owner?(bucket)
-      redirect_to bucket_path(bucket.token), alert: 'Only the bucket owner can perform this operation'
-    end
+    return if owner?(bucket)
+
+    redirect_to bucket_path(bucket.token), alert: 'Only the bucket owner can perform this operation'
   end
 
-  def is_owner?(bucket)
+  def owner?(bucket)
     owner_token == bucket.owner_token || (user_signed_in? && bucket.user == current_user)
   end
 
@@ -70,6 +68,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) do  |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
