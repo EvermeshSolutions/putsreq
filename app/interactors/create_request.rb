0de9ca76c0a0db1773f72bd6ac24ce6aaefc5ecc
Pablo Cantero <pablo@pablocantero.com>
2017-06-18 12:50:49 -0400

    Sanitize request params

--- app/interactors/create_request.rb
+++ app/interactors/create_request.rb
@@ -4,12 +4,22 @@ class CreateRequest
   delegate :bucket, :rack_request, to: :context
 
   def call
-    context.request = bucket.requests.create(body:            rack_request.body.read,
-                                             content_length:  rack_request.content_length,
-                                             request_method:  rack_request.request_method,
-                                             ip:              rack_request.ip,
-                                             url:             rack_request.url,
-                                             headers:         FilterHeaders.call(headers: rack_request.env).headers,
-                                             params:          rack_request.request_parameters)
+    context.request = bucket.requests.create(
+      body:           rack_request.body.read,
+      content_length: rack_request.content_length,
+      request_method: rack_request.request_method,
+      ip:             rack_request.ip,
+      url:            rack_request.url,
+      headers:        FilterHeaders.call(headers: rack_request.env).headers,
+      params:         filter_params(rack_request.request_parameters)
+    )
+  end
+
+  private
+
+  def filter_params(params)
+    params.select do |key, value|
+      key =~ /^[\w\[\]\-]*\z/
+    end
   end
 end
