2c3cec42b00f53a7f9785d0a094efd1e395e5b74
Pablo Cantero <pablo@pablocantero.com>
2017-07-03 07:57:37 -0400

    Trying to fix unsupported encoding
    See https://rollbar.com/putsreq/putsreq/items/8/

--- app/interactors/create_request.rb
+++ app/interactors/create_request.rb
@@ -5,7 +5,7 @@ class CreateRequest
 
   def call
     context.request = bucket.requests.create(
-      body:           rack_request.body.read,
+      body:           rack_request.body.read.force_encoding(Encoding::UTF_8),
       content_length: rack_request.content_length,
       request_method: rack_request.request_method,
       ip:             rack_request.ip,
