5d40d23f24f7adc27a25bd66276f93d8d54c5e34
Pablo Cantero <pablo@pablocantero.com>
2017-08-04 12:25:52 -0400

    Do not save body for multipart/form-data

--- app/interactors/create_request.rb
+++ app/interactors/create_request.rb
@@ -5,7 +5,7 @@ class CreateRequest
 
   def call
     context.request = bucket.requests.create(
-      body:           rack_request.body.read.force_encoding(Encoding::UTF_8),
+      body:           body(rack_request),
       content_length: rack_request.content_length,
       request_method: rack_request.request_method,
       ip:             rack_request.ip,
@@ -17,5 +17,19 @@ class CreateRequest
   rescue
     Rollbar.scope!(request: context.request)
     raise
+  end
+
+  private
+
+  def body(rack_request)
+    return 'multipart/form-data' if multipart_form_data?(rack_request)
+
+    body = rack_request.body.read.force_encoding(Encoding::UTF_8)
+
+    body.is_utf8? ? body : nil
+  end
+
+  def multipart_form_data?(rack_request)
+    rack_request.env['CONTENT_TYPE'].to_s.downcase.include? 'multipart/form-data'
   end
 end
