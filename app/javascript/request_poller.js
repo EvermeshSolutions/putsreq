5bee7a2ce04ed015b7a2b49ccc4604bd34c58748
Pablo Cantero <pablo@pablocantero.com>
2017-12-30 16:06:28 -0500

    Update Pusher (web)

--- app/javascript/request_poller.js
+++ app/javascript/request_poller.js
@@ -3,7 +3,8 @@ import { updateRequestsCount } from './actions'
 export default function startRequestPoller(store) {
   const bucket = $('#putsreq-url-input').data('bucket-token')
 
-  const pusher = new Pusher('1ea9571bd3195cac6d9c', { cluster: 'us2', encrypted: true })
+  const pusher = new Pusher($('body').data('pusher-key'), { cluster: $('body').data('pusher-cluster'), encrypted: true })
+
   const channel = pusher.subscribe(`channel_requests_${bucket}`)
 
   channel.bind('new', (data) => {
