99137dc9f0d5130faff47a35c1cb8d4553edd13e
Pablo Cantero <pablohstc@gmail.com>
2018-04-20 21:01:04 -0400

    Run prettier auto correct

--- app/javascript/actions/index.js
+++ app/javascript/actions/index.js
@@ -1,66 +1,64 @@
-import fetch from "cross-fetch";
-import { bucketsActions } from "../actionTypes";
+import fetch from 'cross-fetch'
+import { bucketsActions } from '../actionTypes'
 
-const getJSONFromPage = id => JSON.parse(document.getElementById(id).innerText);
+const getJSONFromPage = id => JSON.parse(document.getElementById(id).innerText)
 
 const fetchFromPage = () => {
-  const bucket = getJSONFromPage("bucket-json");
-  favicon.badge(bucket.requests_count);
+  const bucket = getJSONFromPage('bucket-json')
+  favicon.badge(bucket.requests_count)
 
   return {
     type: bucketsActions.populate,
     bucket,
     page: 1
-  };
-};
+  }
+}
 
-const favicon = new Favico({ bgColor: "#6C92C8", animation: "none" });
+const favicon = new Favico({ bgColor: '#6C92C8', animation: 'none' })
 
 const updateRequestsCount = count => {
   return (dispatch, getState) => {
     if (count === getState().bucket.requests_count) {
-      return;
+      return
     }
 
-    favicon.badge(count);
+    favicon.badge(count)
 
-    let page = getState().bucket.page || 1;
+    let page = getState().bucket.page || 1
 
     if (count > getState().bucket.request_count) {
-      page = count - getState().bucket.requests_count + page;
+      page = count - getState().bucket.requests_count + page
     }
 
     dispatch({
       type: bucketsActions.updateRequestsCount,
       requests_count: count,
       page: page
-    });
+    })
 
     if (count === 1) {
-      return fetchPage(1)(dispatch, getState);
+      return fetchPage(1)(dispatch, getState)
     }
-  };
-};
+  }
+}
 
 const handlePageChange = page => {
   return (dispatch, getState) => {
-    dispatch({ type: bucketsActions.loading });
+    dispatch({ type: bucketsActions.loading })
 
-    fetchPage(page)(dispatch, getState);
-  };
-};
+    fetchPage(page)(dispatch, getState)
+  }
+}
 
 const fetchPage = page => {
   return (dispatch, getState) => {
     fetch(`${getState().bucket.path}.json?page=${page}`)
       .then(
         response => response.json(),
-        error => console.log("An error occurred.", error)
+        error => console.log('An error occurred.', error)
       )
-      .then(bucket =>
-        dispatch({ type: bucketsActions.populate, bucket, page })
-      );
-  };
-};
+      .then(bucket => dispatch({ type: bucketsActions.populate, bucket, page }))
+  }
+}
 
-export { fetchFromPage, handlePageChange, updateRequestsCount };
+export { fetchFromPage, handlePageChange, updateRequestsCount }
--- app/javascript/store.js
+++ app/javascript/store.js
@@ -3,4 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducers from './reducers'
 
-export default createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)))
+export default createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(thunk))
+)
