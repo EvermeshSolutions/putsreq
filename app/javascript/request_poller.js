ca9aba6b30f58f2f23084b70ff722a1267fa7318
Pablo Cantero <pablohstc@gmail.com>
2018-04-24 11:10:47 -0400

    Revert "Revert "Temporally disable poller""
    This reverts commit 2705df585cd5c3cb672a6cb911660a43c3af8205.

--- app/javascript/request_poller.js
+++ app/javascript/request_poller.js
@@ -3,9 +3,9 @@ import { updateRequestsCount } from './actions'
 export default function startRequestPoller(store) {
   const bucket = $('#putsreq-url-input').data('bucket-token')
 
-  const source = new EventSource(`/${bucket}/requests_count`)
-  source.addEventListener('requests_count', event => {
-    const data = JSON.parse(event.data)
-    updateRequestsCount(data.requests_count)(store.dispatch, store.getState)
-  })
+  // const source = new EventSource(`/${bucket}/requests_count`)
+  // source.addEventListener('requests_count', event => {
+  //   const data = JSON.parse(event.data)
+  //   updateRequestsCount(data.requests_count)(store.dispatch, store.getState)
+  // })
 }
