4d0fe30fa78ee0c86015e4065082fda14893cda1
Pablo Cantero <pablohstc@gmail.com>
2019-06-15 19:05:27 -0400

    Add a specific rate limit response message
    https://github.com/kickstarter/rack-attack#ratelimit-headers-for-well-behaved-clients

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -9,3 +9,16 @@ end
 Rack::Attack.throttle('requests by ip', limit: 45, period: 60) do |request|
   request.ip unless requests_count_path?(request.path)
 end
+
+Rack::Attack.throttled_response = lambda do |env|
+  match_data = env['rack.attack.match_data']
+  now = match_data[:epoch_time]
+
+  headers = {
+    'RateLimit-Limit' => match_data[:limit].to_s,
+    'RateLimit-Remaining' => '0',
+    'RateLimit-Reset' => (now + (match_data[:period] - now % match_data[:period])).to_s
+  }
+
+  [429, headers, ['Your are requests are being temporally throttled. Please contact for increasing limits.\n']]
+end
