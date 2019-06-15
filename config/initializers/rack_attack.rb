12fdaf001f2aab9debd999585875de3f2fd24e33
Pablo Cantero <pablohstc@gmail.com>
2019-06-15 15:10:56 -0400

    Ignore requests_count for throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,2 +1,11 @@
-Rack::Attack.throttle('requests by bucket (path) ', limit: 45, period: 60, &:path)
-Rack::Attack.throttle('requests by ip', limit: 45, period: 60, &:ip)
+def requests_count_path?(path)
+  path.end_with?('/requests_count')
+end
+
+Rack::Attack.throttle('requests by bucket (path) ', limit: 45, period: 60) do |request|
+  request.path unless requests_count_path?(request.path)
+end
+
+Rack::Attack.throttle('requests by ip', limit: 45, period: 60) do |request|
+  request.ip unless requests_count_path?(request.path)
+end
