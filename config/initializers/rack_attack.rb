2f413be04461245a491bf8487d4f4df34970f5e8
Pablo Cantero <pablohstc@gmail.com>
2019-06-14 23:56:21 -0400

    Bump throttle limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,3 +1,2 @@
-Rack::Attack.throttle('requests by bucket (path) ', limit: 5, period: 60, &:path)
-
+Rack::Attack.throttle('requests by bucket (path) ', limit: 30, period: 60, &:path)
 Rack::Attack.throttle('requests by ip', limit: 30, period: 60, &:ip)
