b000a66499a0fc98ca0d67b0cd3dab4e43b389d6
Pablo Cantero <pablohstc@gmail.com>
2019-06-15 17:15:43 -0400

    WIP

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,15 @@
 class RequestsController < ApplicationController
+  def index
+    bucket = Bucket.find_by(token: params[:token])
+
+    requests = bucket.requests
+    requests = requests.gt(id: params[:last_request_id]) if params[:last_request_id].present?
+    requests = requests.limit(25).order(:id.asc)
+    requests = requests.map(&:attributes)
+
+    render json: JSON.generate(requests)
+  end
+
   def show
     request = Request.find(params[:id])
 
