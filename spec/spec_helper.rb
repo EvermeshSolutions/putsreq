209342198ccb52da680b7bd010253831aec8f6f1
Pablo Cantero <pablohstc@gmail.com>
2019-06-16 19:09:12 -0400

    Silence stdout (rspec)
    See https://stackoverflow.com/a/15432948/464685

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,4 +50,17 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include StubEnv::Helpers
+
+  # https://stackoverflow.com/a/15432948/464685
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
