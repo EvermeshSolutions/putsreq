32ac6981cab504c3b343753659934132fa8abd1c
Pablo Cantero <pablohstc@gmail.com>
2019-06-16 21:39:02 -0400

    :lipstick:

--- spec/lib/putsreq/cli_helper_spec.rb
+++ spec/lib/putsreq/cli_helper_spec.rb
@@ -4,34 +4,39 @@ require 'putsreq/cli_helper'
 module PutsReq
   RSpec.describe CLIHelper do
     let(:token) { 'i7kOKCEEO66kR1n0ASzY' }
-    let(:id) { 'j7kOKCEEO66kR1n0ASzY' }
     let(:to) { 'http://example.com' }
     let(:local) { false }
     let(:body) { 'Hello World' }
     let(:headers) { { 'ACCEPT' => 'text/plain' } }
+    let(:request_id) { 'j7kOKCEEO66kR1n0ASzY' }
+    let(:request) do
+      {
+        '_id' => {
+          '$oid' => request_id
+        },
+        'headers' => headers,
+        'body' => body,
+        'request_method' => 'get'
+      }
+    end
 
     subject { described_class.new(token, to, local) }
 
     describe '#subscribe_and_forward' do
       it 'subscribes and forwards' do
-        request = {
-          '_id' => {
-            '$oid' => id
-          },
-          'headers' => headers,
-          'body' => body,
-          'request_method' => 'get'
-        }
-
         expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/last.json").and_return(
           double(code: 200, ok?: true, parsed_response: request)
         )
-        expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/requests.json?last_request_id=#{id}").and_return(
+        expect(HTTParty).to receive(:get).with(
+          "https://putsreq.com/#{token}/requests.json?last_request_id=#{request_id}"
+        ).and_return(
           double(code: 200, ok?: true, parsed_response: [request])
         )
         expect(HTTParty).to receive(:get).with(to, headers: headers, body: body).and_return(OpenStruct.new(code: 200))
 
-        expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/requests.json?last_request_id=#{id}").and_return(
+        expect(HTTParty).to receive(:get).with(
+          "https://putsreq.com/#{token}/requests.json?last_request_id=#{request_id}"
+        ).and_return(
           double(code: 200, ok?: false)
         )
 
@@ -40,16 +45,12 @@ module PutsReq
 
       context 'when not found' do
         it 'exits' do
-          request = {
-            '_id' => {
-              '$oid' => id
-            }
-          }
-
           expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/last.json").and_return(
             double(code: 200, ok?: true, parsed_response: request)
           )
-          expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/requests.json?last_request_id=#{id}").and_return(
+          expect(HTTParty).to receive(:get).with(
+            "https://putsreq.com/#{token}/requests.json?last_request_id=#{request_id}"
+          ).and_return(
             double(code: 200, ok?: false)
           )
           expect(HTTParty).to_not receive(:get)
@@ -61,30 +62,22 @@ module PutsReq
 
     describe '#find_and_forward' do
       it 'forwards a request' do
-        request = {
-          'headers' => headers,
-          'body' => body,
-          'request_method' => 'get'
-        }
-
-        expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/requests/#{id}.json").and_return(
+        expect(HTTParty).to receive(:get).with(
+          "https://putsreq.com/#{token}/requests/#{request_id}.json"
+        ).and_return(
           double(code: 200, ok?: true, parsed_response: request)
         )
         expect(HTTParty).to receive(:get).with(to, headers: headers, body: body).and_return(OpenStruct.new(code: 200))
 
-        subject.find_and_forward(id)
+        subject.find_and_forward(request_id)
       end
 
       context 'when not found' do
         it 'does not forward' do
           id = 'j7kOKCEEO66kR1n0ASzY'
-          request = {
-            'headers' => headers,
-            'body' => body,
-            'request_method' => 'get'
-          }
-
-          expect(HTTParty).to receive(:get).with("https://putsreq.com/#{token}/requests/#{id}.json").and_return(
+          expect(HTTParty).to receive(:get).with(
+            "https://putsreq.com/#{token}/requests/#{request_id}.json"
+          ).and_return(
             double(code: 200, ok?: false, parsed_response: request)
           )
           expect(HTTParty).to_not receive(:get)
